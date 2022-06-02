a9fd7a65dced3641aa749c676cea53c74d6ea59e
Kirillov <kirillov04@yandex.ru>
2022-06-02 17:49:43 +0300

    fix lint

---
+++ brain_games/scripts/brain_even.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+
+from brain_games.cli import welcome_user
+
+
+def main():
+    print('Welcome to the Brain Games!111')
+    welcome_user()
+
+
+if __name__ == '__main__':
+    main()
