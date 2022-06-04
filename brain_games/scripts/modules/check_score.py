3b0e857b4170a3ff052257dc5cd0e34b499a056e
Aleksandr Kirillov <tegos_9898@mail.ru>
2022-06-04 10:51:27 +0300

    some fixes

--- brain_games/scripts/modules/check_score.py
+++ brain_games/scripts/modules/check_score.py
@@ -4,10 +4,8 @@ def check_score(score, res, name):  # example (True, answer, 'no')
     if res[0]:
         score += 1
         print('Correct!')
-        print('-' * 20)
         return score
     else:
         print(f'"{res[1]}" is wrong answer ;(.Correct answer was "{res[2]}"')
         print(f'Let\'s try again, {name}!')
-        print('-' * 20)
         return 0
